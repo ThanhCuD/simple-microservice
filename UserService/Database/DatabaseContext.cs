a5b886a33ec289958fe20a4f8a90b80322e01d89
ThanhCuD <thanhduc552018@gmail.com>
2021-09-13 12:24:27 +0700

    first commit

---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext  : DbContext
+    {
+        public DbSet<User> Users{ get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=WS-THANHDUC\MSSQLSERVER2017;initial catalog=user_microservice;Trusted_Connection=True;");
+        }
+    }
+}
